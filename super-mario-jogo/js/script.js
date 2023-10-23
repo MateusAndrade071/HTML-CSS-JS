977f53451214ebcb68f12b862a89bcba73eceaab
Mateus Adrade <128805906+MateusAndrade071@users.noreply.github.com>
2023-10-23 18:33:00 -0300

    Update script.js

--- super-mario-jogo/js/script.js
+++ super-mario-jogo/js/script.js
@@ -2,14 +2,14 @@ const mario = document.querySelector('.mario')
 const pipe = document.querySelector('.pipe')
 const numero = document.getElementById('numero')
 let contador = 0
-let isColised = false
+let isColised = false // Variável para controlar a colisão, inicialmente definida como falso
 
 const jump = () => {
-    if(!isColised){
-        mario.classList.add('jump')
+    if(!isColised){ // Verifica se a colisão não ocorreu
+        mario.classList.add('jump') // Adiciona a classe 'jump' para animar o salto
 
         setTimeout(() => {
-            mario.classList.remove('jump')
+            mario.classList.remove('jump') // Remove a classe 'jump' após 500ms para encerrar a animação
         } , 500);
     }
 }
@@ -20,28 +20,28 @@ const loop = setInterval(() => {
     
     if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
 
-        pipe.style.animation = 'none';
-        pipe.style.left = `${pipePosition}px`;
+        pipe.style.animation = 'none'; // Para a animação do cano
+        pipe.style.left = `${pipePosition}px`; // Mantém a posição do cano constante
         
-        mario.style.animation = 'none';
-        mario.style.bottom = `${marioPosition}px`;
+        mario.style.animation = 'none'; // Para a animação do Mario
+        mario.style.bottom = `${marioPosition}px`; // Mantém a posição do Mario constante
 
-        mario.src = 'images/game-over.png'
-        mario.style.width = '75px'
-        mario.style.marginLeft = '50px'
+        mario.src = 'images/game-over.png'; // Altera a imagem do Mario para a de "game over"
+        mario.style.width = '75px'; // Ajusta o tamanho do Mario
+        mario.style.marginLeft = '50px'; // Ajusta a margem do Mario
 
-        clearInterval(loop);
-        isColised = true
+        clearInterval(loop); // Encerra o loop principal
+        isColised = true // Define a colisão como verdadeira
     }
 }, 10);
 
 document.addEventListener('keydown', jump);
 
 function adicionarScore(){
-    if(!isColised){
-        contador++;
-        numero.textContent = contador
+    if(!isColised){  // Verifica se a colisão não ocorreu
+        contador++; // Incrementa o contador de pontuação
+        numero.textContent = contador // Atualiza a exibição da pontuação
     }
 }
 
-const loopInfinito = setInterval(adicionarScore, 100);
+const loopInfinito = setInterval(adicionarScore, 100); // Inicia um loop para adicionar pontuação continuamente
